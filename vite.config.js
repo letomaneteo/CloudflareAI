709f7bb83473520f7aedc3bc3c21c3bccc290d0e
letomaneteo <95133883+letomaneteo@users.noreply.github.com>
2026-05-13 17:59:42 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: "automatic"
-    })
-  ]
+  plugins: [react()],
+  build: {
+    target: "es2015"
+  }
 });
