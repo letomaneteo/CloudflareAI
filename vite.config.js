d812b97a969e2a7f30061fc570e3590f36852a60
letomaneteo <95133883+letomaneteo@users.noreply.github.com>
2026-05-13 18:07:52 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: "automatic"
+      jsxRuntime: "classic"
     })
-  ]
+  ],
+  esbuild: {
+    jsxInject: `import React from 'react'`
+  }
 });
